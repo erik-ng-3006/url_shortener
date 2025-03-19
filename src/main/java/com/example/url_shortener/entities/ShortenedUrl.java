33cabb8faef4499cf38f421b123774565c5b396e
erik-ng-3006 <erikng3006@gmail.com>
2025-03-19 12:59:11 +0700

    create shortened url entity

---
+++ src/main/java/com/example/url_shortener/entities/ShortenedUrl.java
@@ -0,0 +1,31 @@
+package com.example.url_shortener.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "shortened_urls")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortenedUrl {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String originalUrl;
+    private String shortCode;
+    
+    @Column(nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+}
+
